6f80978a82626a8b9ab2026cbe3ae7126f3933b0
titosco <titobiloluwatella@gmail.com>
2023-10-09 18:16:45 +0100

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 6-print_matrix_integer.py
+
+def print_matrix_integer(matrix=[[]]):
+    """prints a matrix of integers"""
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+        print()
