381a5ca14a6875147900733791bc4b5747476b8d
titosco <titobiloluwatella@gmail.com>
2023-10-11 22:29:55 +0100

    gittt

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-#0-square_matrix_simple.py
+# 0-square_matrix_simple.py
 
 def square_matrix_simple(matrix=[]):
 	new_matrix = matrix.copy()
 
 	for i in range(len(matrix)):
 		new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
-		
 	return (new_matrix)
