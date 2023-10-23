4a16557843e97fcdeffa9ebad9b0395cb1f97f8d
titosco <titobiloluwatella@gmail.com>
2023-10-23 21:57:59 +0100

    4-list_division.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,16 +2,12 @@
 # 2-safe_print_list_integers.py
 
 def safe_print_list_integers(my_list=[], x=0):
-    counting = 0
-<<<<<<< HEAD
+    counts = 0
     for i in range(0, x):
-=======
-    for i in range(x):
->>>>>>> 950e41079db3aa42559f2d5c2a8706c6adca37f1
         try:
             print("{:d}".format(my_list[i]), end="")
-            counting += 1
+            counts += 1
         except (ValueError, TypeError):
             pass
-        print()
-        return counting
+    print()
+    return counts
