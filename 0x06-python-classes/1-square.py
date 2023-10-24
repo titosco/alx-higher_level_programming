5baabab4891b9c429dcd7190ec0c29e9a1e132db
titosco <titobiloluwatella@gmail.com>
2023-10-24 23:51:51 +0100

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 1-square.py
+
+"""Square module"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size):
+        """Constructor
+        Args:
+                size: lenght of side of the square"""
+        self.__size = size
