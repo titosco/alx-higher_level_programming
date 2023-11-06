e61b7609db0681943fb820690c693550e6e1e592
titosco <titobiloluwatella@gmail.com>
2023-11-06 23:29:02 +0100

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 6-base_geometry.py
+"""module for base_geometry"""
+
+
+class BaseGeometry:
+    """a BaseGeometry class"""
+
+    def area(self):
+        """method to compute area"""
+        raise Exception('area() is not implemented')
