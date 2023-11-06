4d71c0e700982ed0e5ac136ce930d154b5bf364b
titosco <titobiloluwatella@gmail.com>
2023-11-06 23:02:25 +0100

    2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 2-is_same_class.py
+"""module for is_same_class method"""
+
+
+def is_same_class(obj, a_class):
+    """determines if obj is == instance of class"""
+    return type(obj) == a_class
