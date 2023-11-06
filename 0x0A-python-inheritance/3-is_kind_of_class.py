39f7d7528aa6bcab8a0a21b8f5f408f7956c99cd
titosco <titobiloluwatella@gmail.com>
2023-11-06 23:17:20 +0100

    main.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 3-is_kind_of_class.py
+"""module for kind_of_class method"""
+
+
+def is_kind_of_class(obj, a_class):
+    """determine is an object is asubclass of a class"""
+    return isinstance(obj, a_class)
