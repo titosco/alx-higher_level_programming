47f6a5d967b2b58ddae088212ebf41fa596bdb4b
titosco <titobiloluwatella@gmail.com>
2023-11-08 00:48:09 +0100

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 1-write_file.py
+"""Write_file module."""
+
+
+def write_file(filename="", text=""):
+    """Write a string to a UTF8 text file.
+    """
+    with open(filename, "w", encoding="utf-8") as my_file:
+        return my_file.write(text)
