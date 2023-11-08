daa6007757d9a0f47c750b8d5cf86bffe21b178a
titosco <titobiloluwatella@gmail.com>
2023-11-08 01:05:00 +0100

    8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 8-class_to_json.py
+""" My class module
+"""
+
+
+class MyClass:
+    """ My class
+    """
+
+    def __init__(self, name):
+        self.name = name
+        self.number = 0
+
+    def __str__(self):
+        return "[MyClass] {} - {:d}".format(self.name, self.number)
