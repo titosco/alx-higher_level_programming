c6e68e56310d7740e6f309e1fee5d4f7611936e2
titosco <titobiloluwatella@gmail.com>
2024-01-27 01:27:25 +0100

    adding files

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Python script that sends a POST request to the URL and
+to an URL with the letter as a parameter
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    data = {'q': ""}
+
+    try:
+        data['q'] = sys.argv[1]
+    except Exception as e:
+        pass
+
+    r = requests.post('http://0.0.0.0:5000/search_user', data)
+
+    try:
+        json_o = r.json()
+        if not json_o:
+            print("No result")
+        else:
+            print("[{}] {}".format(json_o.get('id'), json_o.get('name')))
+    except Exception as e:
+        print("Not a valid JSON")
